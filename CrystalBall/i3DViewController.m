b50ff58fb171abd3c49f2c01e78eeed40f979383
Iyad Horani <i3d@ironic3d.com>
2014-07-23 16:52:01 +1000

    Added random display of predictions when pressing the predict button

--- CrystalBall/i3DViewController.m
+++ CrystalBall/i3DViewController.m
@@ -10,6 +10,7 @@
 
 @interface i3DViewController ()
 
+@property (strong, nonatomic) NSArray *predictions;
 @property (strong, nonatomic) IBOutlet UILabel *predictionLabel;
 - (IBAction)buttonPressed:(id)sender;
 
@@ -20,7 +21,15 @@
 - (void)viewDidLoad
 {
     [super viewDidLoad];
-	// Do any additional setup after loading the view, typically from a nib.
+    // Do any additional setup after loading the view, typically from a nib.
+
+    self.predictions = @[@"It is Certain",
+            @"It is Decidedly so",
+            @"The stars are not aligned",
+            @"My reply is no",
+            @"Better not tell you now",
+            @"Concentrate and ask again",
+            @"Unable to answer now"];
 }
 
 - (void)didReceiveMemoryWarning
@@ -30,6 +39,8 @@
 }
 
 - (IBAction)buttonPressed:(id)sender {
-    self.predictionLabel.text = @"HOLA!";
+
+    int random = arc4random_uniform(self.predictions.count);
+    self.predictionLabel.text = [self.predictions objectAtIndex:random];
 }
 @end
